5b2836ce097fb5d13e8446afefcb90a050ff9ddb
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2023-01-21 22:37:31 +0200

    less 4 any

--- index.ts
+++ index.ts
@@ -2,15 +2,18 @@ let isBirthdayData:boolean = true;
 let ageData:number = 20;
 let userNameData:string = 'Max';
 
-// function logBrtMsg(isBirthday:boolean, age:number, userName:string):string {    
-//     if (isBirthday) {
-//         return `Congrats ${userName.toLocaleUpperCase()}, age: ${age + 1}`;
-//     } else {
-//         return 'Error';
-//     }
-// }
+let salary:number;
+salary = 5000;
 
-const logBrtMsg = (isBirthday:boolean, age:number, userName:string):string => {    
+const userData = `{"isBirthdayData": true, "ageData": 40, "userNameData": "Max"}`;
+const userObj: {
+    isBirthdayData:boolean;
+    ageData:number;
+    userNameData:string;
+} = JSON.parse(userData)
+console.log(userObj)
+
+function logBrtMsg(isBirthday:boolean, age:number, userName:string):string {    
     if (isBirthday) {
         return `Congrats ${userName.toLocaleUpperCase()}, age: ${age + 1}`;
     } else {
