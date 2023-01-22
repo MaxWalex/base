fc0cb186f23cfb1dd510a7dae773cfbb46c9b690
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2023-01-22 23:35:24 +0200

    tuples

--- index.ts
+++ index.ts
@@ -1,49 +1,52 @@
-const electricityUserData = {
-	readings: 95,
-	units: "kWt",
-	mode: "double",
-};
-
-const waterUserData = {
-	readings: 3,
-	units: "m3",
-};
-
-const elRate:number = 0.45;
-const wRate:number = 2;
-
-const monthPayments:number[] = [0, 0]; // [electricity, water]
-
-const calculatePayments = (
-	{mode, readings}:{mode:string,readings:number}, 
-	wData:{readings:number}, 
-	elRate:number, 
-	wRate:number):void => {
-	if (mode === "double" && readings < 50) {
-		monthPayments[0] = readings * elRate * 0.7;
-	} else {
-		monthPayments[0] = readings * elRate;
+const userData = {
+	isBirthday: true,
+	age: 20,
+	userName: 'Max',
+	messages: {
+		error: 'Error',
 	}
+}
 
-	monthPayments[1] = wData.readings * wRate;
-};
+const userDataTuple:[boolean, number, ...string[]] = [true, 40, "John", 'Max', 'Alex']
+// userDataTuple[3]
+// userDataTuple.push(50)
+// userDataTuple[3]
 
-calculatePayments(electricityUserData, waterUserData, elRate, wRate);
+const res = userDataTuple.map((t) => `${t} - data`)
 
-const sendInvoice = (
-	[el, water]:number[], 
-	{units, readings}:{units:string,readings:number},
-	wData:{readings:number, units:string}):string => {
+const [bthd, age, userName] = userDataTuple;
 
-	const text:string = `    Hello!
-    This month you used ${readings} ${units} of electricity
-    It will cost: ${el}€
-    
-    This month you used ${wData.readings} ${wData.units} of water
-    It will cost: ${water}€`;
+console.log(res)
 
-	return text;
-};
+// const createError = (msg:string) => {
+// 	throw new Error(msg);
+// }
 
-const invoice = sendInvoice(monthPayments, electricityUserData, waterUserData);
-console.log(invoice);
+// const logBrtMsg = ({isBirthday, age, userName, messages:{error}}:{
+// 	isBirthday:boolean,
+// 	age:number,
+// 	userName:string,
+// 	messages:{error:string}
+// }):string => {    
+//     if (isBirthday) {
+//         return `Congrats ${userName.toLocaleUpperCase()}, age: ${age + 1}`;
+//     } else {
+//         return createError(error);
+//     }
+// }
+
+// console.log(logBrtMsg(userData))
+
+// const departments:string[] = ['dev', 'design', 'marketing']
+
+// const department = departments[0]
+
+// // departments.push(5);
+// const report = departments	
+// 						.filter((d:string) => d !== 'dev')
+// 						.map((d:string) => `${d} - done`)
+
+// const nums:number[][] = [[1, 2, 3], [1, 2, 3]]
+
+// const [first, second] = report;
+// console.log(first, second)
