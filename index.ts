77e98bf18a0c011624266af06d64b1023aef793d
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2023-01-22 16:42:18 +0200

    type never

--- index.ts
+++ index.ts
@@ -2,23 +2,20 @@ let isBirthdayData:boolean = true;
 let ageData:number = 20;
 let userNameData:string = 'Max';
 
-let salary:number;
-salary = 5000;
+const createError = (msg:string) => {
+	throw new Error(msg);
+}
 
-const userData = `{"isBirthdayData": true, "ageData": 40, "userNameData": "Max"}`;
-const userObj: {
-    isBirthdayData:boolean;
-    ageData:number;
-    userNameData:string;
-} = JSON.parse(userData)
-console.log(userObj)
-
-function logBrtMsg(isBirthday:boolean, age:number, userName:string):string {    
-    if (isBirthday) {
+const logBrtMsg = (isBirthday:boolean, age:number, userName:string):string => {    
+    if (isBirthday === true) {
         return `Congrats ${userName.toLocaleUpperCase()}, age: ${age + 1}`;
-    } else {
-        return 'Error';
+    } else if (isBirthday === false) {
+        return 'Too bad';
     }
+	
+	return createError('Error');
 }
 
 logBrtMsg(isBirthdayData, ageData, userNameData)
+
+// const smth:never = null
